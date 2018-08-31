d8a7a059e16aa716085c3d07e8a05a7697ad1704
Trygve Lie <post@trygve-lie.com>
2018-08-31 16:49:47 +0200

    Reworking wireframe document for development

--- example/multiple-podlet-server/menu.js
+++ example/multiple-podlet-server/menu.js
@@ -6,12 +6,12 @@ const Podlet = require('../../');
 class Menu {
     constructor(pathname) {
         const podlet = new Podlet({
-            defaults: true,
             pathname,
             fallback: '/fallback',
             version: `2.0.0-${Date.now().toString()}`,
             logger: console,
             name: 'menu',
+            dev: true,
         });
 
         this.app = express.Router(); // eslint-disable-line new-cap
@@ -19,11 +19,43 @@ class Menu {
         this.app.use(podlet.middleware());
 
         this.app.get(podlet.content(), (req, res) => {
-            res.status(200).render('menu.content.njk');
+            const ctx = res.locals.podium.context;
+            res.podiumSend(`<menu>
+                <ul>
+                    <li><a href="${ctx.mountOrigin}${
+                ctx.mountPathname
+            }/section_1/">Menu 1</a></li>
+                    <li><a href="${ctx.mountOrigin}${
+                ctx.mountPathname
+            }/section_2/">Menu 2</a></li>
+                    <li><a href="${ctx.mountOrigin}${
+                ctx.mountPathname
+            }/section_3/">Menu 3</a></li>
+                    <li><a href="${ctx.mountOrigin}${
+                ctx.mountPathname
+            }/section_4/">Menu 4</a></li>
+                    <li><a href="${ctx.mountOrigin}${
+                ctx.mountPathname
+            }/section_5/">Menu 5</a></li>
+                    <li><a href="${ctx.mountOrigin}${
+                ctx.mountPathname
+            }/section_6/">Menu 6</a></li>
+                </ul>
+            </menu>`);
         });
 
         this.app.get(podlet.fallback(), (req, res) => {
-            res.status(200).render('menu.fallback.njk');
+            res.podiumSend(`<menu>
+                <ul>
+                    <li>Fallback Menu 1</li>
+                    <li>Fallback Menu 2</li>
+                    <li>Fallback Menu 3</li>
+                    <li>Fallback Menu 4</li>
+                    <li>Fallback Menu 5</li>
+                    <li>Fallback Menu 6</li>
+                    <li>Fallback Menu 7</li>
+                </ul>
+            </menu>`);
         });
 
         this.app.get(podlet.manifest(), (req, res) => {
--- example/single-podlet-server/server.js
+++ example/single-podlet-server/server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const nunjucks = require('nunjucks');
 const express = require('express');
 const Podlet = require('../../');
 const Proxy = require('@podium/proxy');
@@ -8,12 +7,12 @@ const Proxy = require('@podium/proxy');
 const app = express();
 
 const podlet = new Podlet({
-    defaults: true,
     pathname: '/',
     fallback: '/fallback',
     version: `2.0.0-${Date.now().toString()}`,
     logger: console,
     name: 'podletContent',
+    dev: true,
 });
 
 podlet.defaults({
@@ -24,41 +23,36 @@ const proxy = new Proxy({
     logger: console,
 });
 
-nunjucks.configure(['./views', podlet.views('njk')], {
-    autoescape: true,
-    express: app,
-});
-
 app.use(podlet.middleware());
 app.use(proxy.middleware());
 
 app.get(podlet.content(), (req, res) => {
     if (res.locals.podium.context.locale === 'nb-NO') {
-        res.status(200).render('content.no.njk');
+        res.podiumSend('<h2>Hei verden</h2>');
         return;
     }
-    res.status(200).render('content.en.njk');
+    res.podiumSend('<h2>Hello world</h2>');
 });
 
 app.get(podlet.fallback(), (req, res) => {
-    res.status(200).render('fallback.njk');
+    res.podiumSend('<h2>We are sorry but we can not display this!</h2>');
 });
 
 app.get(podlet.manifest(), (req, res) => {
-    res.status(200).json(podlet);
+    res.json(podlet);
 });
 
 app.get(podlet.proxy('/public', 'localApi'), (req, res) => {
     if (res.locals.podium.context.locale === 'nb-NO') {
-        res.status(200).json({ say: 'hei verden' });
+        res.json({ say: 'Hei verden' });
         return;
     }
-    res.status(200).json({ say: 'hello world' });
+    res.json({ say: 'Hello world' });
 });
 
 app.use('/assets', express.static('assets'));
-podlet.css('http://localhost:7100/assets/module.css');
-podlet.js('http://localhost:7100/assets/module.js');
+podlet.css({ value: '/assets/module.css' });
+podlet.js({ value: '/assets/module.js' });
 
 podlet.proxy('https://api.ipify.org', 'remoteApi');
 
