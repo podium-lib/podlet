d1b484b765419566130a2295a9b6be9f156ffeff
William Killerud <william.killerud@schibsted.com>
2024-07-17 13:23:32 +0200

    docs: make the execution more streaming-robust [skip ci]

--- example/single-podlet-server/assets/module.js
+++ example/single-podlet-server/assets/module.js
@@ -2,20 +2,30 @@
 
 'use strict';
 
-const element = document.getElementById('content');
-const context = {
-    mountOrigin: element.getAttribute('data-mount-origin'),
-    publicPathname: element.getAttribute('data-public-pathname'),
-};
+function doSomething() {
+    const element = document.getElementById('content');
+    const context = {
+        mountOrigin: element.getAttribute('data-mount-origin'),
+        publicPathname: element.getAttribute('data-public-pathname'),
+    };
 
-const base = new URL(
-    context.publicPathname,
-    context.mountOrigin ? context.mountOrigin : window.location.origin,
-);
-const localApiUri = new URL('localApi', base);
+    const base = new URL(
+        context.publicPathname,
+        context.mountOrigin ? context.mountOrigin : window.location.origin,
+    );
+    const localApiUri = new URL('localApi', base);
 
-fetch(localApiUri)
-    .then((response) => response.json())
-    .then((obj) => {
-        element.innerHTML = `Proxied local REST API says: ${obj.say}`;
-    });
+    fetch(localApiUri)
+        .then((response) => response.json())
+        .then((obj) => {
+            element.innerHTML = `Proxied local REST API says: ${obj.say}`;
+        });
+}
+
+if (document.readyState === 'loading') {
+    // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', doSomething);
+} else {
+    // `DOMContentLoaded` has already fired
+    doSomething();
+}
