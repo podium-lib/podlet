f976981dd9fed4d75cf58becd90f7d042c28d0fd
William Killerud <william.killerud@schibsted.com>
2024-09-23 10:31:06 +0200

    refactor: use same name as elsewhere

--- fixup.js
+++ fixup.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-let fixup = path.join(process.cwd(), 'types', 'fixup.d.ts');
+let fixup = path.join(process.cwd(), 'types', 'podium.d.ts');
 let module = path.join(process.cwd(), 'types', 'podlet.d.ts');
 
 fs.writeFileSync(
